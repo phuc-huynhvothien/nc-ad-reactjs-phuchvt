0a1ef4ef4076c1c91813b505566d34bef98d2cd0
huynhvothienphuc963 <huynhvothienphuc9631@gmail.com>
2020-06-25 20:41:53 +0700

    Lab1

--- hello-word.js
+++ hello-word.js
@@ -1,12 +1,14 @@
 var text = "Hellow World";
 console.log(text);
 // Annotations
-var message = "Hello";
-var age = 31;
+var alertMessage = "Hello";
+var myNumber = 31;
 var isOK = true;
 var files;
 var student;
 files = ["Hello day 1", "Hello dude"];
 student = { id: 100.1, name: "Phuc HVT" };
+alertMessage = alertMessage + "This is alert";
+console.log(alertMessage);
 console.log(files);
 console.log(student);
--- hello-word.ts
+++ hello-word.ts
@@ -2,14 +2,26 @@ let text: string = "Hellow World";
 console.log(text);
 
 // Annotations
-let message: string = "Hello";
-const age: number = 31;
-const isOK: boolean = true;
+const myNumber: number = 31;
+let isOK: boolean = true;
+let isNo: boolean = true;
 let files: string[];
-let student: { id: number; name: string };
-
+let human: { id: number; name: string; isHuman: boolean };
+// Default value ? 
 files = ["Hello day 1", "Hello dude"]
-student = { id: 100.1, name: "Phuc HVT" };
+human = { id: 100.1, name: "Phuc HVT", isHuman: true };
 
+
+
+isOK = human.isHuman ? true : false;
+// isOK = isNo?: false;
+let alertMessage: string = "Hello";
+alertMessage = alertMessage + " This is alert";
+// human.name = alertMessage?:"NOPE" ; 
+// ==> Expression excepted
+
+
+console.log(alertMessage);
+files.pop();
 console.log(files);
-console.log(student);
+console.log(human);
