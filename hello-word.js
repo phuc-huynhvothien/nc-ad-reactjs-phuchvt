0367c0f4d8db4dd91dccfc762fb3c81cebb189d4
huynhvothienphuc963 <huynhvothienphuc9631@gmail.com>
2020-06-25 20:05:41 +0700

    add file config

--- hello-word.js
+++ hello-word.js
@@ -1,2 +1,12 @@
-var text = "Hellow Word";
+var text = "Hellow World";
 console.log(text);
+// Annotations
+var message = "Hello";
+var age = 31;
+var isOK = true;
+var files;
+var student;
+files = ["Hello day 1", "Hello dude"];
+student = { id: 100.1, name: "Phuc HVT" };
+console.log(files);
+console.log(student);
--- hello-word.ts
+++ hello-word.ts
@@ -1,2 +1,15 @@
-let text: string = "Hellow Word";
+let text: string = "Hellow World";
 console.log(text);
+
+// Annotations
+let message: string = "Hello";
+const age: number = 31;
+const isOK: boolean = true;
+let files: string[];
+let student: { id: number; name: string };
+
+files = ["Hello day 1", "Hello dude"]
+student = { id: 100.1, name: "Phuc HVT" };
+
+console.log(files);
+console.log(student);
